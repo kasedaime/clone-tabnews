07d533608c98e4ae7509c80e8ebcb3ae2398937e
kasedaime <30650934+kasedaime@users.noreply.github.com>
2023-11-04 13:54:11 +0000

    adiciona scripts `lint:check` e `lint:fix`

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # clone-tabnews
+
 Implementação educacional do tabnews.com.br
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
 function Home() {
-    return <h1>Amo-te imenso! Da uma risada</h1>
+  return <h1>Amo-te imenso! Da uma risada</h1>;
 }
 
 export default Home;
